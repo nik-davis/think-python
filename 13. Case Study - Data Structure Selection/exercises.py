294e796038d02bb4822f4358972617a15bdbffc8
Nik Davis <nik.davis@hotmail.co.uk>
2018-09-13 15:20:20 +0100

    Setup ex 13.5 choose_from_hist

--- 13. Case Study - Data Structure Selection/exercises.py
+++ 13. Case Study - Data Structure Selection/exercises.py
@@ -249,11 +249,29 @@ def ex13_4():
     print(rest)
 
     
+# 13.5.  Write a function named choose_from_hist that takes a histogram 
+# as defined in Section 11.2 and returns a random value from the histogram, 
+# chosen with probability in proportion to frequency. 
+# 
+# For example, for this histogram:
+#
+#	>>> t = ['a', 'a', 'b']
+#	>>> hist = histogram(t)
+#	>>> hist
+#	{'a': 2, 'b': 1}
+#	
+# your function should return 'a' with probability 2/3 and 'b' with probability 1/3.
 
+
+import random
+
+for i in range(10):
+    x = random.random()
+    print(x)
 
 
 # Run solutions
 # ex13_1()
-ex13_2()
-ex13_3()
-ex13_4()
+# ex13_2()
+# ex13_3()
+# ex13_4()
