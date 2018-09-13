23bf94810f45930c9c88f3273e6859181db143b4
Nik Davis <nik.davis@hotmail.co.uk>
2018-09-13 12:42:34 +0100

    Begin ex 13.2: Book read

--- 13. Case Study - Data Structure Selection/exercises.py
+++ 13. Case Study - Data Structure Selection/exercises.py
@@ -54,6 +54,66 @@ def ex13_1():
     # Uncomment to print word list:
     # print(t)
 
+# 13.2. Go to Project Gutenberg (http://gutenberg.org ) and download your
+# favorite out-of-copyright book in plain text format.
+#
+# Modify your program from the previous exercise to read the book you
+# downloaded, skip over the header information at the beginning of the 
+# file, and process the rest of the words as before.
+#
+# Then modify the program to count the total number of words in the book,
+# and the number of times each word is used.
+#
+# Print the number of different words used in the book. Compare different
+# books by different authors, written in different eras. Which author uses
+# the most extensive vocabulary?
+
+
+def get_words(filename):
+    '''Returns a lower-cased list of words from a given file with
+    punctuation and whitespace removed
+
+    filename: path of file to read
+
+    returns: list of strings
+    '''
+    s = open_file(filename)
+    
+    t = s.split()
+
+    for i in range(len(t)):
+        t[i] = t[i].lower()
+        t[i] = t[i].strip(string.punctuation)
+
+    return t
+
+
+def ex13_2():
+    '''Run ex 13.2 solution'''
+
+    t = get_words('resources/grimm.txt')
+    f = open('resources/grimm.txt', encoding='UTF-8').readlines()
+    s = str()
+
+    write = False
+    for line in f:
+        if line [:7] == '*** END':
+            write = False
+
+        if write:
+            s += line
+
+        if line[:12] == '*** START OF':
+            write = True
+
+    print(s)
+    
+        
+
+    # Uncomment to print word list:
+    # print(t)
 
 # Run solutions
-ex13_1()
+# ex13_1()
+ex13_2()
+
